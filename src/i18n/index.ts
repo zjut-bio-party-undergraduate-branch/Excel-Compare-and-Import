097ccf711abd096e3a7f9a59cafad97d0a5388f9
qww <497363953@qq.com>
2023-08-12 18:27:21 +0800

    format

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,7 +1,7 @@
 import { createI18n } from 'vue-i18n'
 import { en } from './en'
 import { zh } from './zh'
-import {bitable} from '@base-open/web-api'
+import { bitable } from '@base-open/web-api'
 
 export const i18n = createI18n({
   locale: 'en',
