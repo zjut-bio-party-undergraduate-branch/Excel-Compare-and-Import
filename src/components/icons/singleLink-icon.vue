05e3135bb09bc2d6448ae6a8c05e868bce3ad1cb
qww <497363953@qq.com>
2023-09-17 15:01:29 +0800

    build: icons

---
+++ src/components/icons/singleLink-icon.vue
@@ -0,0 +1,14 @@
+<template>
+  <svg
+    className="icon"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M4 1a2 2 0 0 0-2 2v13a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2H4Zm10 2v13H4V3h10Zm8.982 13.513a.504.504 0 0 0 .018-.136V11.5a.5.5 0 0 0-.9-.3l-3.135 4.18a.5.5 0 0 0 .292.788l1.713.38A5 5 0 0 1 16 21h-2.5a1 1 0 1 0 0 2H16a7 7 0 0 0 6.982-6.487Z"
+    />
+  </svg>
+</template>
