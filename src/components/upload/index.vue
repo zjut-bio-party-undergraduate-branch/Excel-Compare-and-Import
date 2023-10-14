18f30cbba631778312bce543f66438ecf498bc7c
qww <497363953@qq.com>
2023-10-14 08:32:34 +0800

    refactor: upload

--- src/components/upload/index.vue
+++ src/components/upload/index.vue
@@ -1,112 +1,113 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import { Upload, Close } from "@element-plus/icons-vue";
-import { genFileId, ElMessage } from "element-plus";
-import type { UploadInstance, UploadFile, UploadRawFile } from "element-plus";
-import * as XLSX from "xlsx";
-import type { ExcelDataInfo } from "@/types/types";
-import { useI18n } from "vue-i18n";
-import uploadIcon from "@/components/icons/upload-icon.vue";
-import excelIcon from "@/components/icons/excel-icon.vue";
+import { ref } from "vue"
+import { Upload, Close } from "@element-plus/icons-vue"
+import { genFileId, ElMessage } from "element-plus"
+import type { UploadInstance, UploadFile, UploadRawFile } from "element-plus"
+import * as XLSX from "xlsx"
+import type { ExcelDataInfo, SheetInfo } from "@/types/types"
+import { useI18n } from "vue-i18n"
+import uploadIcon from "@/components/icons/upload-icon.vue"
+import excelIcon from "@/components/icons/excel-icon.vue"
+import { useFileReader } from "@qww0302/use-bitable"
 
-const upload = ref<UploadInstance>();
-const data = ref<ExcelDataInfo | null>(null);
-const { t } = useI18n();
+const upload = ref<UploadInstance>()
+const { t } = useI18n()
+const excelFile = ref<File | null>(null)
 
-async function readExcel(file: UploadFile): Promise<ExcelDataInfo | null> {
-  if (!file) return null;
-  if (!/\.(xls|xlsx)$/.test(file.name.toLowerCase())) return null;
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      try {
-        const data = e.target?.result;
-        const workbook = XLSX.read(data, { type: "binary" });
-        const sheets = workbook.SheetNames.map((name) => {
-          const sheet = workbook.Sheets[name];
-          const tableData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
-          if (tableData.length <= 1) return null;
-
-          try {
-            // @ts-ignore
-            const fields = tableData[0]?.map((name: string) => ({
-              name: String(name),
-            }));
-            const records = tableData
-              .slice(1)
-              // @ts-ignore
-              .map((row: string[]) => {
-                const record: { [key: string]: string } = {};
-                row.forEach((value, index) => {
-                  record[fields[index].name] = value ? String(value) : "";
-                });
-                return record;
+const { data, pending, name } = useFileReader<ExcelDataInfo | null>(excelFile, {
+  load: (data, resolve) => {
+    try {
+      const workbook = XLSX.read(data, { type: "binary" })
+      const sheets = workbook.SheetNames.map((name) => {
+        const sheet = workbook.Sheets[name]
+        const tableData = XLSX.utils.sheet_to_json(sheet, {
+          header: 1,
+        }) as any[][]
+        if (tableData.length <= 1) return null
+        try {
+          const fields = tableData[0]?.map((name: string) => ({
+            name: String(name),
+          }))
+          const records = tableData
+            .slice(1)
+            .map((row: string[]) => {
+              const record: { [key: string]: string } = {}
+              row.forEach((value, index) => {
+                record[fields[index].name] = value ? String(value) : ""
               })
-              .filter((record) => {
-                return Object.values(record).some((value) => {
-                  return value;
-                });
-              });
-            return { name, tableData: { fields, records } };
-          } catch (e) {
-            console.log(e);
-            ElMessage.error(t("message.sheetError", { sheetName: name }));
-            return null;
-          }
-
-          // console.log({ name, tableData: { fields, records } })
-        }).filter((sheet) => sheet !== null);
-        //@ts-ignore
-        resolve({ sheets, name: file.name });
-      } catch (e) {
-        reject(e);
+              return record
+            })
+            .filter((record) => {
+              return Object.values(record).some((value) => {
+                return value
+              })
+            })
+          return { name, tableData: { fields, records } }
+        } catch (e) {
+          console.log(e)
+          ElMessage.error(t("message.sheetError", { sheetName: name }))
+          return null
+        }
+      }).filter((sheet) => sheet !== null) as SheetInfo[]
+      if (sheets.length === 0) {
+        ElMessage.error(t("message.noSheet"))
+        excelFile.value = null
+        resolve(null)
+        return
       }
-    };
-    reader.readAsBinaryString(file.raw as Blob);
-  });
-}
+      resolve({ sheets, name: (excelFile.value as File).name })
+    } catch (e) {
+      console.error(e)
+    }
+  },
+})
 
-async function analyzeExcel(file: UploadFile): Promise<void> {
-  data.value = await readExcel(file);
-  console.log("readExcel", data.value);
+function getFile(file: UploadFile) {
+  excelFile.value = file.raw as File
 }
 
 function exceedHandler(files: File[]): void {
-  upload.value!.clearFiles();
-  const file = files[0] as UploadRawFile;
-  file.uid = genFileId();
-  upload.value!.handleStart(file);
+  upload.value!.clearFiles()
+  const file = files[0] as UploadRawFile
+  file.uid = genFileId()
+  upload.value!.handleStart(file)
 }
 
 function closeFileCard(): void {
-  data.value = null;
-  upload.value!.clearFiles();
+  excelFile.value = null
+  upload.value!.clearFiles()
 }
 
 defineExpose({
   data,
-});
+})
 </script>
 
 <template>
   <h3>
-    <el-icon><Upload /></el-icon>
+    <el-icon>
+      <Upload />
+    </el-icon>
     {{ t("h.upload") }}
   </h3>
   <el-upload
-    v-show="!data"
+    v-show="!excelFile"
     ref="upload"
     drag
-    :on-change="analyzeExcel"
+    :on-change="getFile"
     accept=".xls,.xlsx"
     :limit="1"
     :auto-upload="false"
     :on-exceed="exceedHandler"
     :show-file-list="false"
   >
-    <el-icon color="#2962f1" size="30" class="el-icon--upload"
-      ><uploadIcon style="color: var(--color)"
-    /></el-icon>
+    <el-icon
+      color="#2962f1"
+      size="30"
+      class="el-icon--upload"
+    >
+      <uploadIcon style="color: var(--color)" />
+    </el-icon>
     <div class="el-upload__text">
       <el-text>{{ t("upload.tip.common") }}</el-text>
     </div>
@@ -114,18 +115,30 @@ defineExpose({
       <el-text size="small">{{ t("upload.tip.fileSupport") }}</el-text>
     </div>
   </el-upload>
-  <el-card v-show="data" shadow="hover">
+  <el-card
+    v-loading="pending"
+    v-show="excelFile"
+    shadow="hover"
+  >
     <el-row>
       <el-col :span="4">
-        <el-icon size="2em"><excelIcon /></el-icon>
+        <el-icon size="2em">
+          <excelIcon />
+        </el-icon>
       </el-col>
       <el-col :span="18">
-        <el-space style="height: 100%" alignment="center">
-          <el-text truncated>{{ data?.name }}</el-text>
+        <el-space
+          style="height: 100%"
+          alignment="center"
+        >
+          <el-text truncated>{{ name }}</el-text>
         </el-space>
       </el-col>
       <el-col :span="2">
-        <el-button text @click="closeFileCard">
+        <el-button
+          text
+          @click="closeFileCard"
+        >
           <el-icon>
             <close />
           </el-icon>
