85f975db7bb575dc81a5822ba6c6d03cc9a78347
qww <497363953@qq.com>
2023-12-17 00:46:15 +0800

    test: add test

--- src/utils/cellValue/location.ts
+++ src/utils/cellValue/location.ts
@@ -4,94 +4,22 @@ import {
   IOpenLocation,
 } from "@lark-base-open/js-sdk"
 import { defineTranslator, type AsyncParams, FieldRole } from "./cell"
-import { unique, batch } from "@/utils"
+import {
+  unique,
+  batch,
+  geo,
+  regeo,
+  isLatLongitude,
+  type LocationResponse,
+} from "@/utils"
 import type { fieldMap } from "@/types/types"
 
-let locationCache: Record<string, IOpenLocation> = {}
+export let locationCache: Record<string, IOpenLocation> = {}
 
-interface LocationResponse {
-  record_id: string
-  address: string
-  adname: string
-  cityname: string
-  full_address: string
-  location: string
-  name: string
-  pname: string
-}
+export const LIMIT_NUMBER = 50
+export const LIMIT_INTERVAL = 1000
 
-// Thanks to Chris Chou's help for providing the API
-const GEO_URL = "https://base-translator-api.replit.app/base_geo"
-const REGEO_URL = "https://base-translator-api.replit.app/base_regeo"
-const LIMIT_NUMBER = 50
-const LIMIT_INTERVAL = 1000
-
-const geoHeader = new Headers()
-geoHeader.append("Content-Type", "application/json")
-geoHeader.append("Referer", "https://location-utils.plz.click")
-
-/**
- * location text to base location
- *
- * @param locations
- * @returns
- */
-async function geo(locations: string[]) {
-  const fetchOptions = {
-    method: "POST",
-    headers: geoHeader,
-    body: JSON.stringify({
-      brand: "feishu",
-      location_list: locations.map((i) => {
-        return {
-          record_id: i,
-          input: i,
-        }
-      }),
-    }),
-    redirect: "follow" as RequestRedirect,
-  }
-  return fetch(GEO_URL, fetchOptions)
-    .then((response) => response.text())
-    .then((res) => JSON.parse(res) as LocationResponse[])
-}
-
-const regeoHeader = new Headers()
-regeoHeader.append("Content-Type", "application/json")
-regeoHeader.append("Referer", "https://location-utils.plz.click")
-
-/**
- * Latitude and longitude to base location
- *
- * @param locations
- * @returns
- */
-async function regeo(locations: string[]) {
-  const fetchOptions = {
-    method: "POST",
-    headers: regeoHeader,
-    body: JSON.stringify({
-      brand: "feishu",
-      location_list: locations.map((i) => {
-        return {
-          record_id: i,
-          input: i,
-        }
-      }),
-    }),
-    redirect: "follow" as RequestRedirect,
-  }
-  return fetch(REGEO_URL, fetchOptions)
-    .then((response) => response.text())
-    .then((res) => JSON.parse(res) as LocationResponse[])
-}
-
-function isLatLongitude(value: string) {
-  const regoReg = /^(\d+(\.\d+)?),(\d+(\.\d+)?)$/
-  return regoReg.test(value)
-}
-
-async function batchLocation(
+export async function batchLocation(
   locations: string[],
   method: (list: string[]) => Promise<LocationResponse[]>,
   onProgress?: (num: number) => void,
